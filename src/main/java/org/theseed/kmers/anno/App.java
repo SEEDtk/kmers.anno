88903267f8773c07786eb970f20c867f60c8b0c8
Bruce Parrello <bparrello@figresearch.com>
2020-01-30 13:31:13 -0600

    empty initial prototype

---
+++ src/main/java/org/theseed/kmers/anno/App.java
@@ -0,0 +1,18 @@
+package org.theseed.kmers.anno;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Processor runObject = new Processor();
+        boolean ok = runObject.parseCommand(args);
+        if (ok) {
+            runObject.run();
+        }
+    }
+}
---
+++ src/main/java/org/theseed/kmers/anno/Processor.java
@@ -0,0 +1,66 @@
+/**
+ *
+ */
+package org.theseed.kmers.anno;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.theseed.utils.ICommand;
+
+/**
+ * main processing class
+ *
+ * @author Bruce Parrello
+ *
+ */
+public class Processor implements ICommand {
+
+    // COMMAND LINE
+
+    /** help option */
+    @Option(name="-h", aliases={"--help"}, help=true)
+    private boolean help;
+
+    /** TRUE if we want progress messages */
+    @Option(name="-v", aliases={"--verbose", "--debug"}, usage="display progress on STDERR")
+    private boolean debug;
+
+
+    /**
+     * Parse command-line options to specify the parameters of this object.
+     *
+     * @param args	an array of the command-line parameters and options
+     *
+     * @return TRUE if successful, FALSE if the parameters are invalid
+     */
+    public boolean parseCommand(String[] args) {
+        boolean retVal = false;
+        // Set the defaults.
+        this.help = false;
+        this.debug = false;
+        // Parse the command line.
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+            if (this.help) {
+                parser.printUsage(System.err);
+            } else {
+                // TODO parsing body
+                // We made it this far, we can run the application.
+                retVal = true;
+            }
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
+            // For parameter errors, we display the command usage.
+            parser.printUsage(System.err);
+//        } catch (IOException e) {
+//            System.err.println(e.getMessage());
+        }
+        return retVal;
+    }
+
+    public void run() {
+        // TODO execute body
+    }
+}
