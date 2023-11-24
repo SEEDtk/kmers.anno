b9cff6392103e1ec19c94629a920c51040561584
Bruce Parrello <bparrello@figresearch.com>
2023-11-24 13:24:59 -0600

    First working hash annotation.

--- src/main/java/org/theseed/proteins/kmers/anno/HashAnnotationProcessor.java
+++ src/main/java/org/theseed/proteins/kmers/anno/HashAnnotationProcessor.java
@@ -7,62 +7,58 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.time.Duration;
 
-import org.theseed.utils.BaseInputProcessor;
 import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.theseed.basic.ParseFailureException;
-import org.theseed.io.FieldInputStream;
+import org.theseed.genome.Feature;
+import org.theseed.genome.Genome;
+import org.theseed.genome.iterator.GenomeSource;
 import org.theseed.io.TabbedLineReader;
-import org.theseed.p3api.P3Connection;
-import org.theseed.proteins.kmer.hash.ProteinKmerHashMap;
+import org.theseed.proteins.kmer.hash.GenomeProteinKmers;
+import org.theseed.utils.BaseMultiReportProcessor;
 
 /**
- * This sub-command creates a protein kmer hash and uses it to annotate genomes in a PATRIC
- * genome dump directory.  Such a directory contains a "genome_feature.json" file that has
- * obsolete annotations in it.  The corrected annotations are written to a tab-delimited
- * file of feature IDs and annotations named "anno.tbl".  If no annotation is possible,
- * the old annotation is retained.
+ * This sub-command uses protein kmer hashing to annotate GTOs.  The GTOs should be at least
+ * PROTEIN detail level:  DNA is not required.  For each GTO, a file named "XXXXXX.anno.tbl"
+ * will be output, where "XXXXXX" is the genome ID. If no annotation is possible, the old annotation
+ * is retained.
  *
- * The standard input should contain a protein annotation file.  This is a tab-delimited file
- * with headers, and the proteins are in a column named "protein", the annotations in a column
- * named "annotation".
+ * The annotation data is in a role annotation file.  This is a tab-delimited file with headers, and
+ * the proteins are in a column named "protein" and the annotations in a column named "annotation".
  *
- * The positional parameter should be the name of the PATRIC genome dump directory.  This is a
- * directory of sub-directories, each directory containing JSON dump files for one genome.
+ * The positional parameters should be the name of the role annotation file and a genome source.
+ *
+ * Our strategy is to read in all the kmers for a genome, then process the entire role annotation file
+ * to find the best match for each incoming peg.
  *
  * The command-line options are as follows:
  *
  * -h	display command-line usage
  * -v	display more frequent log messages
- * -i	input file containing proteins and annotations (if not STDIN)
- * -K	protein kmer size
- * -b	batch size for load
+ * -K	protein kmer size (default 8)
+ * -D	output directory for annotation files (default "Annotations" in the current directory)
+ * -t	type of genome source (default DIR)
  *
- * --min	minimum similarity score for an annotation to be acceptable
- * --num	estimated number of proteins being input
+ * --minSim		minimum similarity score for an annotation to be acceptable (default 0.005)
+ * --minLen		minimum acceptable protein length (default 50)
  *
  */
-public class HashAnnotationProcessor extends BaseInputProcessor {
+public class HashAnnotationProcessor extends BaseMultiReportProcessor {
 
     // FIELDS
     /** logging facility */
     protected static Logger log = LoggerFactory.getLogger(HashAnnotationProcessor.class);
-    /** protein map for annotations */
-    private ProteinKmerHashMap<String> annoMap;
-    /** list of genome directories */
-    private File[] gDirs;
     /** input column index for protein strings */
     private int protIdx;
     /** input column index for annotation strings */
     private int annoIdx;
+    /** genomes to process */
+    private GenomeSource genomes;
 
     // COMMAND-LINE OPTIONS
 
@@ -70,42 +66,44 @@ public class HashAnnotationProcessor extends BaseInputProcessor {
     @Option(name = "--kmer", aliases = { "-K" }, metaVar = "10", usage = "protein kmer size")
     private int kmerSize;
 
-    /** batch size for load */
-    @Option(name = "--batch", aliases = { "-b" }, metaVar = "100", usage = "batch size for hash loading")
-    private int batchSize;
-
     /** minimum similarity score for an annotation */
-    @Option(name = "--min", metaVar = "0.1", usage = "minimum acceptable similarity score for annotation")
+    @Option(name = "--minSim", metaVar = "0.1", usage = "minimum acceptable similarity score for annotation")
     private double minScore;
 
-    /** estimated number of proteins being hashed */
-    @Option(name = "--num", metaVar = "1000", usage = "estimated number of proteins being put into the hash")
-    private int protExpected;
+    /** minimum length for a usable annotation protein */
+    @Option(name = "--minLen", metaVar = "200", usage = "minimum acceptable length for an annotation protein")
+    private double minProtLen;
 
-    /** input PATRIC genome dump directory */
-    @Argument(index = 0, metaVar = "inDir", usage = "input genome dump directory", required = true)
+    /** type of genome source */
+    @Option(name = "--source", aliases = { "-t" }, usage = "type of genome source")
+    private GenomeSource.Type sourceType;
+
+    /** input role annotation file */
+    @Argument(index = 0, metaVar = "annoFile", usage = "input role annotation file", required = true)
+    private File annoFile;
+
+    /** input genome source */
+    @Argument(index = 1, metaVar = "inDir", usage = "input genome source", required = true)
     private File inDir;
 
     @Override
-    protected void setReaderDefaults() {
+    protected void setMultiReportDefaults() {
         this.kmerSize = 8;
         this.minScore = 0.005;
-        this.batchSize = 5000;
-        this.protExpected = 15000;
+        this.sourceType = GenomeSource.Type.DIR;
+        this.minProtLen = 50;
+    }
+    @Override
+    protected File setDefaultOutputDir(File curDir) {
+        return new File(curDir, "Annotations");
     }
 
     @Override
-    protected void validateReaderParms() throws IOException, ParseFailureException {
+    protected void validateMultiReportParms() throws IOException, ParseFailureException {
         // Insure the kmer size is valid.
         if (this.kmerSize < 2)
             throw new ParseFailureException("Kmer Size must be at least 2.");
         log.info("Kmer size is {}.", this.kmerSize);
-        // Insure the batch size is valid.
-        if (this.batchSize < 1)
-            throw new ParseFailureException("Batch size must be positive.");
-        // Insure the estimated protein count is valid.
-        if (this.protExpected < 10)
-            throw new ParseFailureException("Expected protein count must be at least 10.");
         // Insure the min score is valid.
         if (this.minScore < 0.0 || this.minScore >= 1.0)
             throw new ParseFailureException("Minimum similarity score must be between 0 and 1.");
@@ -113,108 +111,128 @@ public class HashAnnotationProcessor extends BaseInputProcessor {
         // Insure the genome dump directory is valid.
         if (! this.inDir.isDirectory())
             throw new FileNotFoundException("Input directory " + this.inDir + " is not found or invalid.");
-        this.gDirs = this.inDir.listFiles(P3Connection.GENOME_FILTER);
-        if (this.gDirs.length <= 0)
-            throw new FileNotFoundException("No genomes found in input directory " + this.inDir + ".");
-    }
-
-    @Override
-    protected void validateReaderInput(TabbedLineReader reader) throws IOException {
-        this.protIdx = reader.findField("protein");
-        this.annoIdx = reader.findField("annotation");
-    }
-
-    @Override
-    protected void runReader(TabbedLineReader reader) throws Exception {
-        log.info("Creating kmer hash table.");
-        this.annoMap = new ProteinKmerHashMap<String>(this.kmerSize, this.protExpected);
-        // Initialize the load counters.
-        int protCount = 0;
-        int skipCount = 0;
-        int lineCount = 0;
-        // Create the load batch.
-        var batch = new ArrayList<Map.Entry<String, String>>(this.batchSize);
-        // Loop through the input, storing the proteins.
-        for (var line : reader) {
-            lineCount++;
-            String prot = line.get(this.protIdx);
-            String anno = line.get(this.annoIdx);
-            if (StringUtils.isBlank(anno) || StringUtils.isBlank(prot)
-                    || prot.length() < this.kmerSize || anno.contains("hypothetical"))
-                skipCount++;
-            else {
-                if (batch.size() >= this.batchSize) {
-                    // Here the batch is full, so load it.
-                    this.loadBatch(batch);
-                    batch.clear();
-                    log.info("{} lines read, {} proteins kept, {} kmers in table.", lineCount, protCount,
-                            this.annoMap.getKmerCount());
-                }
-                batch.add(new AbstractMap.SimpleEntry<String, String>(prot, anno));
-                protCount++;
-            }
+        // Insure the minimum protein length is valid.
+        if (this.minProtLen < this.kmerSize)
+            throw new FileNotFoundException("Minimum protein length cannot be less than kmer size.");
+        // Validate the role annotation file.  We make sure we can read it and that it has the necessary fields.
+        if (! this.annoFile.canRead())
+            throw new FileNotFoundException("Role annotation file " + this.annoFile + " is not found or unreadable.");
+        try (TabbedLineReader annoStream = new TabbedLineReader(this.annoFile)) {
+            this.annoIdx = annoStream.findField("annotation");
+            this.protIdx = annoStream.findField("protein");
         }
-        // Process the residual batch.
-        if (batch.size() > 0)
-            this.loadBatch(batch);
-        log.info("{} lines read.  {} proteins kept, {} skipped. {} kmers in table.", lineCount,
-                protCount, skipCount, this.annoMap.getKmerCount());
+        // Validate the genome source.
+        if (! this.inDir.exists())
+            throw new FileNotFoundException("Genome source " + this.inDir + " not found.");
+        log.info("Connecting to genome source {} of type {}.", this.inDir, this.sourceType);
+        this.genomes = this.sourceType.create(this.inDir);
+        log.info("{} genomes loaded from {}.", this.genomes.size(), this.inDir);
+    }
+
+    @Override
+    protected void runMultiReports() throws Exception {
         // Now we loop through the genome directories.
+        int gTotal = this.genomes.size();
         int gCount = 0;
-        int foundCount = 0;
         int featCount = 0;
-        protCount = 0;
-        skipCount = 0;
-        long lastMsg = System.currentTimeMillis();
-        log.info("Processing {} genome directories.", this.gDirs.length);
-        for (File gDir : this.gDirs) {
-            String genomeId = gDir.getName();
+        int protCount = 0;
+        int confirmCount = 0;
+        int defaultCount = 0;
+        int newAnnoCount = 0;
+        for (Genome genome : this.genomes) {
+            String genomeId = genome.getId();
             gCount++;
-            log.info("Processing genome {}:  {}.", gCount, genomeId);
-            File featFile = new File(gDir, P3Connection.JSON_FILE_NAME);
-            File annoFile = new File(gDir, "anno.tbl");
-            try (
-                    PrintWriter writer = new PrintWriter(annoFile);
-                    FieldInputStream featStream = FieldInputStream.create(featFile)) {
-                int fidColIdx = featStream.findField("patric_id");
-                int protColIdx = featStream.findField("aa_sequence_md5");
-                int annoColIdx = featStream.findField("product");
-                // Write the output header.
-                writer.println("fid\tannotation");
-                // Loop through the genome features.
-                for (var line : featStream) {
-                    String fid = line.get(fidColIdx);
-                    if (StringUtils.isBlank(fid))
-                        skipCount++;
+            log.info("Processing genome {} of {}:  {}.", gCount, gTotal, genome);
+            try (PrintWriter writer = this.openReport(genomeId + ".anno.tbl")) {
+                long genomeStart = System.currentTimeMillis();
+                // Loop through the genome features, filling the kmer hash.
+                int fCount = 0;
+                int sCount = 0;
+                int pCount = 0;
+                GenomeProteinKmers genomeKmers = new GenomeProteinKmers(this.kmerSize, this.minScore);
+                for (Feature feat : genome.getFeatures()) {
+                    // Get the data for this feature.
+                    String prot = feat.getProteinTranslation();
+                    fCount++;
+                    if (StringUtils.isBlank(prot))
+                        sCount++;
                     else {
-                        String annotation = line.get(annoColIdx);
-                        featCount++;
-                        String prot = line.get(protColIdx);
-                        if (! StringUtils.isBlank(prot)) {
-                            // Here we have a protein to check.
-                            protCount++;
-                            var closest = this.annoMap.findClosest(prot);
-                            if (closest.getSimValue() >= this.minScore) {
-                                foundCount++;
-                                annotation = closest.getValue();
+                        String fid = feat.getId();
+                        String annotation = feat.getPegFunction();
+                        pCount++;
+                        genomeKmers.addProtein(fid, prot, annotation);
+                    }
+                }
+                featCount += fCount;
+                protCount += pCount;
+                log.info("{} kmers for {} proteins in hash for {}.", genomeKmers.getKmerCount(), pCount, genome);
+                log.info("{} features processed, {} skipped, {} proteins", fCount, sCount, pCount);
+                // Now we need to annotate the features.  We do this by reading through the annotation file.
+                try (TabbedLineReader annoStream = new TabbedLineReader(this.annoFile)) {
+                    int closeCount = 0;
+                    int lineCount = 0;
+                    long lastMsg = System.currentTimeMillis();
+                    long start = lastMsg;
+                    // Loop through the annotation stream, performing annotations.
+                    for (var line : annoStream) {
+                        String prot = line.get(this.protIdx);
+                        String anno = line.get(this.annoIdx);
+                        lineCount++;
+                        // Insure this is a useful annotation.
+                        if (! StringUtils.isBlank(anno) && prot.length() >= this.minProtLen) {
+                            int count = genomeKmers.processProposal(prot, anno);
+                            closeCount += count;
+                            if (log.isInfoEnabled() && System.currentTimeMillis() - lastMsg >= 5000) {
+                                lastMsg = System.currentTimeMillis();
+                                double rate = lineCount * 1000.0 / (lastMsg - start);
+                                log.info("{} annotation lines read, {} matches found, {} lines/second.", lineCount, closeCount, rate);
                             }
                         }
-                        writer.println(fid + "\t" + annotation);
                     }
-                    if (log.isInfoEnabled() && System.currentTimeMillis() - lastMsg >= 10000) {
-                        log.info("{} features processed, [} skipped, {} proteins, {} new annotations found.",
-                                featCount, skipCount, protCount, foundCount);
-                        lastMsg = System.currentTimeMillis();
+                    // Write the output header.
+                    writer.println("fid\tscore\tnew_annotation\told_annotation");
+                    // Now unspool the features to the output.  Note that we will leave the score blank if we
+                    // have a non-protein feature.
+                    int cCount = 0;
+                    int naCount = 0;
+                    int dCount = 0;
+                    for (Feature feat : genome.getFeatures()) {
+                        String md5 = feat.getMD5();
+                        String oldAnnotation = feat.getPegFunction();
+                        GenomeProteinKmers.Proposal proposal = null;
+                        if (! StringUtils.isBlank(md5))
+                            proposal = genomeKmers.getProposal(md5);
+                        // At this point, a NULL proposal means the feature was not annotated.
+                        if (proposal == null)
+                            writer.println(feat.getId() + "\t\t" + oldAnnotation + "\t" + oldAnnotation);
+                        else {
+                            // Here we have a successful annotation.  Determine the type (default, confirm, new).
+                            double score = proposal.getSim();
+                            String newAnnotation = proposal.getAnnotation();
+                            if (score == 0.0)
+                                dCount++;
+                            else if (oldAnnotation.contentEquals(newAnnotation))
+                                cCount++;
+                            else
+                                naCount++;
+                            writer.println(feat.getId() + "\t" + String.valueOf(score) + "\t" + newAnnotation
+                                    + "\t" + oldAnnotation);
+                        }
                     }
+                    if (log.isInfoEnabled()) {
+                        Duration time = Duration.ofMillis(System.currentTimeMillis() - genomeStart);
+                        log.info("{} default annotations, {} confirmed annotations, {} new annotations in {}.",
+                                dCount, cCount, naCount, genome);
+                        log.info("{} to annotation {}.", time, genome);
+                    }
+                    confirmCount += cCount;
+                    defaultCount += dCount;
+                    newAnnoCount += naCount;
                 }
             }
+            log.info("{} total proteins out of {} features processed for {} genomes.", protCount, featCount, gCount);
+            log.info("{} annotations confirmed, {} updated, {} defaulted.", confirmCount, newAnnoCount, defaultCount);
         }
-        log.info("{} features processed, [} skipped, {} proteins, {} new annotations found.",
-                featCount, skipCount, protCount, foundCount);
-    }
-
-    private void loadBatch(ArrayList<Entry<String, String>> batch) {
-        batch.parallelStream().forEach(x -> this.annoMap.addProtein(x.getKey(), x.getValue()));
     }
 
 }
