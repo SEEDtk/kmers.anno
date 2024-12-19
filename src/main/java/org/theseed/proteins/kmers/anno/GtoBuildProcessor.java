24bc1a6a515c5b4ba6b53de6a17fe5f253ddb276
Bruce Parrello <bparrello@figresearch.com>
2024-12-19 10:54:43 -0600

    Adapted GtoBuildProcessor to use a genome source.

--- src/main/java/org/theseed/proteins/kmers/anno/GtoBuildProcessor.java
+++ src/main/java/org/theseed/proteins/kmers/anno/GtoBuildProcessor.java
@@ -12,39 +12,40 @@ import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.theseed.basic.ParseFailureException;
 import org.theseed.genome.Genome;
+import org.theseed.genome.iterator.GenomeSource;
 import org.theseed.io.TabbedLineReader;
-import org.theseed.p3api.P3Connection;
-import org.theseed.p3api.P3Genome;
 import org.theseed.utils.BaseMultiReportProcessor;
 import org.theseed.genome.Feature;
 
 /**
  * This command is used to apply new annotations and protein families to old genomes in the BV-BRC. We will use
- * the standard P3Genome object to load genomes from the SOLR data, erase the protein families and gene names, and
+ * a genome source to load the desired genomes, erase the protein families and gene names, and
  * then update the annotations, gene names, and local protein families from the input files. The input files are
  * in a single directory and consist of the following files, all of which are tab-delimited without headers.
  *
- * calls					new annotations (column 2) for each feature ID (column 1)
- * genome.names				list of genomes to process (column 1)
- * local.family.members		protein family index number (column 1) and gene name (column 5) for each feature ID (column 2)
+ * calls								new annotations (column 2) for each feature ID (column 1)
+ * local.family.members.expanded		protein family index number (column 1) and gene name (column 5) for each feature ID (column 2)
+ * local.family.defs					function (column 2) for each protein family index number (column 1)
  *
  * An ID will be computed for each family, consisting of the prefix "PLF_", the genus ID, another
  * underscore ("_"), and an eight-digit index number.
  *
- * The positional parameters are the genus number to use in the pseudo-IDs and the name of the input directory
- * containing the annotation files.
+ * The positional parameters are the genus number to use in the pseudo-IDs, the name of the input directory
+ * containing the annotation files, and the name of the input genome source (file or directory)
  *
  * The command-line options are as follows:
  *
  * -h	display command-line usage
  * -v	display more frequent log messages
  * -D	output directory name (default is "gtos" in the current directory)
+ * -t	type of genome source (default PATRIC)
  *
- * --clear	erase the output directory before processing
+ * --clear		erase the output directory before processing
  *
  * @author Bruce Parrello
  *
@@ -56,20 +57,26 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
     protected static Logger log = LoggerFactory.getLogger(GtoBuildProcessor.class);
     /** map of genome IDs to GTOs */
     private Map<String, Genome> genomeMap;
+    /** map of protein family IDs to functions */
+    private Map<String, String> familyMap;
     /** protein family ID prefix */
     private String genusIdPrefix;
     /** name of annotation file */
     private File annoFile;
-    /** name of genome name file */
-    private File genomeFile;
     /** name of protein family file */
     private File familyFile;
-    /** connection to PATRIC */
-    private P3Connection p3;
+    /** name of family function file */
+    private File functionFile;
+    /** input genome source */
+    private GenomeSource genomes;
     /** genus ID validation pattern */
     private static final Pattern GENUS_ID_PATTERN = Pattern.compile("[1-9][0-9]*");
 
     // COMMAND-LINE OPTIONS
+
+    /** type of genome source */
+    @Option(name = "--type", aliases = { "--source", "-t" }, usage = "type of input genome source")
+    private GenomeSource.Type sourceType;
 
     /** genus ID for the input genomes */
     @Argument(index = 0, metaVar = "genus_id", usage = "numeric genus ID for the input genomes", required = true)
@@ -79,6 +86,10 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
     @Argument(index = 1, metaVar = "inDir", usage = "input directory name for protein family / annotation files", required = true)
     private File inDir;
 
+    /** input genome source (file or directory) */
+    @Argument(index = 2, metaVar = "genomeDir", usage = "input genome source (file or directory")
+    private File genomeDir;
+
     @Override
     protected File setDefaultOutputDir(File curDir) {
         return new File(curDir, "gtos");
@@ -86,6 +97,7 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
 
     @Override
     protected void setMultiReportDefaults() {
+        this.sourceType = GenomeSource.Type.PATRIC;
     }
 
     @Override
@@ -102,49 +114,44 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
         this.annoFile = new File(this.inDir, "calls");
         if (! this.annoFile.canRead())
             throw new FileNotFoundException("Cannot find the annotation file " + this.annoFile + " in the input directory.");
-        this.genomeFile = new File(this.inDir, "genome.names");
-        if (! this.genomeFile.canRead())
-            throw new FileNotFoundException("Cannot find the genome name file " + this.genomeFile + " in the input directory.");
-        this.familyFile = new File(this.inDir, "local.family.members");
+        this.familyFile = new File(this.inDir, "local.family.members.expanded");
         if (! this.familyFile.canRead())
             throw new FileNotFoundException("Cannot find the family list file " + this.familyFile + " in the input directory.");
-        // Connect to PATRIC.
-        log.info("Connecting to PATRIC.");
-        this.p3 = new P3Connection();
-        // Create the genome map.
-        this.genomeMap = new HashMap<String, Genome>();
+        this.functionFile = new File(this.inDir, "local.family.defs");
+        if (! this.functionFile.canRead())
+            throw new FileNotFoundException("Cannot find the family definition file " + this.functionFile + " in the input directory.");
+        // Connect to the genome source.
+        this.genomes = this.sourceType.create(this.genomeDir);
+        log.info("{} genomes found in source {}.", this.genomes.size(), this.genomeDir);
+        // Create the genome map and family map.
+        this.genomeMap = new HashMap<String, Genome>(this.genomes.size() * 3 / 2 + 1);
+        this.familyMap = new HashMap<String, String>();
     }
 
     @Override
     protected void runMultiReports() throws Exception {
-        // Loop through the genome file, reading the GTOs. For each GTO, we clear the gene name, annotation, and protein family data
-        // for each feature.
-        try (TabbedLineReader genomeStream = new TabbedLineReader(this.genomeFile, 2)) {
-            log.info("Reading genomes from {}.", this.genomeFile);
-            // Count the genomes read and the proteins erased.
-            int gCount = 0;
-            int pCount = 0;
-            for (var line : genomeStream) {
-                String genomeId = line.get(0);
-                String genomeName = line.get(1);
-                gCount++;
-                log.info("Reading genome #{} {}: {}", gCount, genomeId, genomeName);
-                Genome genome = P3Genome.load(this.p3, genomeId, P3Genome.Details.FULL);
-                // Clean the genome. We need to erase the gene names, the protein families, and the annotated functions.
-                for (Feature feat : genome.getFeatures()) {
-                    if (feat.isProtein()) {
-                        feat.setFunction("hypothetical protein");
-                        feat.setPgfam(null);
-                        feat.setPlfam(null);
-                        feat.setGeneName("");
-                        pCount++;
-                    }
+        // Loop through the genome source, loading it into memory. For each genome, we clear the gene
+        // name, annotation, and protein family data for each feature.
+        int gCount = 0;
+        int pCount = 0;
+        for (Genome genome : this.genomes) {
+            gCount++;
+            log.info("Processing genome #{}: {}", gCount, genome);
+            String genomeId = genome.getId();
+            // Clean the genome. We need to erase the gene names, the protein families, and the annotated functions.
+            for (Feature feat : genome.getFeatures()) {
+                if (feat.isProtein()) {
+                    feat.setFunction("hypothetical protein");
+                    feat.setPgfam(null);
+                    feat.setPlfam(null);
+                    feat.setGeneName("");
+                    pCount++;
                 }
-                // Put the genome in the map.
-                this.genomeMap.put(genomeId, genome);
             }
-            log.info("{} genomes read, {} proteins cleared.", gCount, pCount);
+            // Put the genome in the map.
+            this.genomeMap.put(genomeId, genome);
         }
+        log.info("{} genomes read, {} proteins cleared.", gCount, pCount);
         // Read the annotation file and update the annotations.
         try (TabbedLineReader annoStream = new TabbedLineReader(this.annoFile, 4)) {
             // Count the features annotated and the number of bad feature IDs.
@@ -173,12 +180,25 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
             }
             log.info("{} total features annotated, {} total errors.", aCount, errCount);
         }
+        // Read the family definitions. These are used to annotate any proteins that are in families.
+        try (TabbedLineReader defStream = new TabbedLineReader(this.functionFile, 6)) {
+            log.info("Saving protein family functions from {}.", this.functionFile);
+            for (var line : defStream) {
+                // Get the family index and function.
+                String famIdx = line.get(0);
+                String function = line.get(1);
+                String famId = this.familyId(famIdx);
+                this.familyMap.put(famId,  function);
+            }
+            log.info("{} family definitions read from {}.", this.familyMap.size(), this.functionFile);
+        }
         // Read the family file and update the gene names and protein family definitions.
         try (TabbedLineReader famStream = new TabbedLineReader(this.familyFile, 5)) {
             // Count the families stored, the gene names stored, and the number of bad feature IDs.
             int fCount = 0;
-            int gCount = 0;
+            gCount = 0;
             int errCount = 0;
+            int funCount = 0;
             log.info("Applying protein families from {}.", this.familyFile);
             long lastMsg = System.currentTimeMillis();
             // Loop through the family specifications.
@@ -191,9 +211,16 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
                 else {
                     // Form the protein family ID.
                     String famIdx = line.get(0);
-                    String plfam = this.genusIdPrefix + StringUtils.leftPad(famIdx, 8, '0');
+                    String plfam = this.familyId(famIdx);
+                    // Store the local family ID.
                     feat.setPlfam(plfam);
                     fCount++;
+                    // Store the family function.
+                    String function = this.familyMap.get(plfam);
+                    if (function != null) {
+                        feat.setFunction(function);
+                        funCount++;
+                    }
                     // Check for a gene name.
                     String gene = line.get(4);
                     if (! StringUtils.isBlank(gene)) {
@@ -203,11 +230,11 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
                 }
                 long now = System.currentTimeMillis();
                 if (now - lastMsg >= 10000) {
-                    log.info("{} families updated, {} gene names stored, {} errors.", fCount, gCount, errCount);
+                    log.info("{} families updated, {} gene names stored, {} functions stored, {} errors.", fCount, gCount, funCount, errCount);
                     lastMsg = now;
                 }
             }
-            log.info("{} total families updated, {} total gene names stored, {} total errors.", fCount, gCount, errCount);
+            log.info("{} total families updated, {} total gene names stored, {} total functions stored, {} total errors.", fCount, gCount, funCount, errCount);
         }
         // Finally, we save the GTOs to the output directory.
         for (Genome genome : this.genomeMap.values()) {
@@ -215,6 +242,15 @@ public class GtoBuildProcessor extends BaseMultiReportProcessor {
             log.info("Saving {} to {}.", genome, gFile);
             genome.save(gFile);
         }
+    }
+
+    /**
+     * @return the full protein family ID for a family index
+     *
+     * @param famIdx	family index
+     */
+    private String familyId(String famIdx) {
+        return this.genusIdPrefix + StringUtils.leftPad(famIdx, 8, '0');
     }
 
     /**
